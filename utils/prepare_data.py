c76c0e29d2b3412bf458b13bc4dc1af9c0ddefa6
rado <radoslav.yankov@tum.de>
2024-11-10 18:59:59 +0100

    fix data format

--- utils/prepare_data.py
+++ utils/prepare_data.py
@@ -3,6 +3,7 @@ import numpy as np
 from plyfile import PlyData, PlyElement
 import json
 import os
+from pathlib import Path
 from nerfstudio.data.utils.colmap_parsing_utils import qvec2rotmat, read_cameras_binary, read_images_binary, read_points3D_binary
 
 
@@ -22,9 +23,22 @@ def create_ply_file(bin_path):
 
 def create_transforms(bin_path):
     images = read_images_binary(os.path.join(bin_path, 'images.bin'))
+    camera = read_cameras_binary(os.path.join(bin_path, 'cameras.bin'))
+    camera_list = list(camera.values())
 
+    print(camera_list[0].params)
     # Define JSON structure
     transforms_data = {
+        "w": camera_list[0].width,  # Image width
+        "h": camera_list[0].height,  # Image height
+        "fl_x": camera_list[0].params[0],       # Focal length in x-direction
+        "fl_y": camera_list[0].params[1],       # Focal length in y-direction
+        "cx": camera_list[0].params[2],         # Principal point in x
+        "cy": camera_list[0].params[3],         # Principal point in y
+        "k1": 0,         # Radial distortion coefficient
+        "k2": 0,         # Radial distortion coefficient
+        "p1": 0,         # Tangential distortion coefficient
+        "p2": 0,          # Tangential distortion coefficient
         "frames": []
     }
 
@@ -32,40 +46,51 @@ def create_transforms(bin_path):
 
     # Populate frames with camera poses and image paths
     for image_id, image in images.items():
-        # Convert quaternion to rotation matrix
-        rotation_matrix = qvec2rotmat(image.qvec)
-        #rotation_matrix = image.rotation_matrix()
-        
-        # Combine rotation and translation to form a 4x4 transformation matrix
-        transform_matrix = np.hstack((rotation_matrix, image.tvec.reshape(3, 1)))
-        transform_matrix = np.vstack((transform_matrix, [0, 0, 0, 1]))  # Homogeneous coordinates
+        rotation = qvec2rotmat(image.qvec)
 
-        transforms_data["frames"].append({
-            "file_path": f"images/{image.name}",  # Adjust path to match your dataset structure
-            "transform_matrix": transform_matrix.tolist()
-        })
+        translation = image.tvec.reshape(3, 1)
+        w2c = np.concatenate([rotation, translation], 1)
+        w2c = np.concatenate([w2c, np.array([[0, 0, 0, 1]])], 0)
+        c2w = np.linalg.inv(w2c)
+        # Convert from COLMAP's camera coordinate system (OpenCV) to ours (OpenGL)
+        c2w[0:3, 1:3] *= -1
+        
+        c2w = c2w[np.array([0, 2, 1, 3]), :]
+        c2w[2, :] *= -1
+
+        name = image.name
+        name = Path(f"./images/{name}")
+
+        frame = {
+            "file_path": name.as_posix(),
+            "transform_matrix": c2w.tolist(),
+            "colmap_im_id": image_id,
+        }
+        transforms_data["frames"].append(frame)
 
     # Save to transforms.json
     with open('transforms.json', 'w') as f:
         json.dump(transforms_data, f, indent=4)
 
 def create_base_cam(bin_path):
+    # not implemented yet do not use
     camera = read_cameras_binary(os.path.join(bin_path, 'cameras.bin'))
-    #camera = next(cameras.values())  # Assuming one camera
     camera_list = list(camera.values())
 
-    # Create JSON for base_cam
+    
+    # Create JSON for base_cam - single camera
     base_cam_data = {
         "width": camera_list[0].width,
         "height": camera_list[0].height,
-        "fl_x": camera_list[0].fx,       # Focal length in x-direction
-        "fl_y": camera_list[0].fy,       # Focal length in y-direction
-        "cx": camera_list[0].cx,         # Principal point in x
-        "cy": camera_list[0].cy,         # Principal point in y
-        "k1": camera_list[0].k1,         # Radial distortion coefficient
-        "k2": camera_list[0].k2,         # Radial distortion coefficient
-        "p1": camera_list[0].p1,         # Tangential distortion coefficient
-        "p2": camera_list[0].p2          # Tangential distortion coefficient
+        # parameters not avaialble in the camera dictionary
+        #"fl_x": camera_list[0].fx,       # Focal length in x-direction
+        #"fl_y": camera_list[0].fy,       # Focal length in y-direction
+        #"cx": camera_list[0].cx,         # Principal point in x
+        #"cy": camera_list[0].cy,         # Principal point in y
+        #"k1": camera_list[0].k1,         # Radial distortion coefficient
+        #"k2": camera_list[0].k2,         # Radial distortion coefficient
+        #"p1": camera_list[0].p1,         # Tangential distortion coefficient
+        #"p2": camera_list[0].p2          # Tangential distortion coefficient
     }
 
     # Save to base_cam.json
@@ -74,4 +99,7 @@ def create_base_cam(bin_path):
 
 if __name__ == '__main__':
     bin_path = 'data/alameda/colmap/sparse/0'
-    create_base_cam(bin_path)
+
+    create_ply_file(bin_path)
+    create_transforms(bin_path)
+    #create_base_cam(bin_path)
