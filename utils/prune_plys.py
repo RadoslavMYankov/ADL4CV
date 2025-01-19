4210f27bf494de524b881a1d0bf88867f2707bd6
rado <radoslav.yankov@tum.de>
2025-01-19 10:42:35 +0100

    fix error prone pruning step

--- utils/prune_plys.py
+++ utils/prune_plys.py
@@ -1,6 +1,18 @@
 from merge_plys import downsample
 import open3d as o3d
 import argparse
+import numpy as np
+
+def nerf_cs_to_colmap(nerf_pcd):
+    applied_transform = np.array([
+        [1.0, 0.0,  0.0,  0.0],
+        [0.0, 0.0,  1.0,  0.0],
+        [-0.0, -1.0, -0.0, -0.0],
+        [0.0, 0.0,  0.0,  1.0]
+    ], dtype=np.float64)
+
+    nerf_pcd.transform(applied_transform)
+    return nerf_pcd
 
 def merge_and_prune(sfm_input, nerf_inputs, num_points, output_path):
     sfm_pcd = o3d.io.read_point_cloud(sfm_input)
@@ -10,12 +22,14 @@ def merge_and_prune(sfm_input, nerf_inputs, num_points, output_path):
     
     nerf_pcd = o3d.io.read_point_cloud(nerf_inputs)
     nerf_pcd = downsample(nerf_pcd, num_nerf_points)
+    nerf_pcd = nerf_cs_to_colmap(nerf_pcd)
     merged_pcd = sfm_pcd + nerf_pcd
     o3d.io.write_point_cloud(output_path, merged_pcd)
 
 def prune(path_to_pc, num_points, output_path):
     pc = o3d.io.read_point_cloud(path_to_pc)
     pc = downsample(pc, num_points)
+    pc = nerf_cs_to_colmap(pc)
     o3d.io.write_point_cloud(output_path, pc)
 
 if __name__ == '__main__':
@@ -32,6 +46,8 @@ if __name__ == '__main__':
     #print(args.merge)
 
     if args.merge:
+        print("Merging and pruning")
         merge_and_prune(args.sfm_input, args.path_to_nerf_pc, args.num_points, args.output_path)
     else:
+        print("Pruning")
         prune(args.path_to_nerf_pc, args.num_points, args.output_path)
